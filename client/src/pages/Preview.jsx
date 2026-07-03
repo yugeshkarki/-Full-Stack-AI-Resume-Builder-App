6f233e1d03d11d245214f2bb2646e9c076425d2d
yugesh karki <yugeshkarki1@gmail.com>
2026-07-03 16:11:37 +0545

    frontend complete

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets';
+import ResumePreview from '../components/ResumePreview.jsx';
+import { ArrowLeftIcon, Loader } from 'lucide-react';
+
 
 const Preview = () => {
-  return (
-    <div>
-      <h1>Preview Page</h1>
+  const {resumeId}= useParams()
+
+const[isLoading,setIsLoading]=useState(true);
+  const[resumeData, setResumeData]= useState(null);
+
+   
+
+  const loadResume= async()=>{
+    setResumeData(dummyResumeData.find(resume=> resume._id=== resumeId || null));
+    setIsLoading(false);
+  }
+  useEffect(()=>{
+   loadResume()
+  },[])
+  return resumeData ?(
+    <div className='bg-slate-100'>
+       <div className='max-w-3xl mx-auto py-10'>
+      <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color} classes="py-4 bg-white"/>
+       </div>
     </div>
+  ):(
+   <div>
+    {isLoading ? <Loader/>:(
+      <div className='flex flex-col items-center justify-center h-screen'>
+        <p className='text-center text-6xl text-slate-400 font-medium'> Resume not found</p>
+        <a href='/' className='mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex-items-center transition-colors'>
+          <ArrowLeftIcon className='mr-2 size-4'/>go to home page
+        </a>
+      </div>
+    )}
+   </div>
   )
 }
 
--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -51,6 +51,27 @@ const {resumeId}=useParams();
   useEffect(()=>{
     loadExistingResume()
   },[])
+
+   const changeResumeVisibility= async()=>{
+    setResumeData({...resumeData, public: !resumeData.public})
+   }
+   
+   const handleShare=()=>{
+  const frontendUrl=window.location.href.split('/app')[0];
+  const resumeUrl=frontendUrl+ '/view/' + resumeId;
+
+    if(navigator.share){
+      navigator.share({url:resumeUrl, text:"my Resume"})
+    }
+    else{
+      alert("Share not supported on this browser");
+    }
+   }
+
+   const downloadResume=()=>{
+    window.print();
+   }
+
   return (
     <div>
       <div className='max-w-7xl mx-auto px-4 py-6'>
@@ -132,18 +153,18 @@ const {resumeId}=useParams();
      <div className='relative w-full'>
      <div className='absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2'>
       {resumeData.public  && (
-        <button  className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors'>
-          <Share2Icon className='size-4'/>
+        <button onClick={handleShare} className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors'>
+          <Share2Icon className='size-4'/>Share
 
         </button>
       )}
-      <button className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600 rounded-lg ring-purple-300 hover:ring transition-colors'>
+      <button onClick={changeResumeVisibility} className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600 rounded-lg ring-purple-300 hover:ring transition-colors'>
         {resumeData.public ? <EyeIcon className='size-4'/>:
         <EyeOffIcon className='size-4'/>
         }
         {resumeData.public ? 'public':'private'}
       </button>
-      <button className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-green-100 to-purple-200 text-green-600 rounded-lg ring-green-300 hover:ring transition-colors'>
+      <button onClick={downloadResume} className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-green-100 to-purple-200 text-green-600 rounded-lg ring-green-300 hover:ring transition-colors'>
         <DownloadIcon className='size-4'/>Download
       </button>
       </div>
